0312fecf26df37e46ea61e668679e28a35bca976
Marcusvi13 <marcusvi13@gmail.com>
2026-09-20 23:16:28 +0200

    Afegeix la final de la Supercopa al calendari web

--- calendar-mail.js
+++ calendar-mail.js
@@ -15,6 +15,7 @@
     ['2026-09-11','11/09/2026 · 18:30','Lliga Catalana ACB · Grup 2','Asisa Joventut – FIATC Girona','95–84',true,'https://msstats.optimalwayconsulting.com/v1/fcbq/matches/85572674-2cf8-466b-94e9-d7274f9e5c05/stats/pdf?period=4'],
     ['2026-09-13','13/09/2026 · 12:00','Lliga Catalana ACB · Final · si es classifica','Asisa Joventut – Rival pendent','',false,''],
     ['2026-09-19','19/09/2026 · 18:00','Supercopa Endesa · Semifinal','Asisa Joventut – Kosner Baskonia','',false,''],
+    ['2026-09-20','20/09/2026 · 19:00','Supercopa Endesa · Final','Asisa Joventut – Barça','',false,''],
     ['2026-09-27','27/09/2026 · 12:00','Liga Endesa · J1','Río Breogán – Asisa Joventut','',false,''],
     ['2026-10-03','03/10/2026 · 18:00','Liga Endesa · J2','Asisa Joventut – MoraBanc Andorra','',false,''],
     ['2026-10-06','06/10/2026','BCL · Grup H · J1','Asisa Joventut – Bnei Penlink Herzliya','',false,''],
